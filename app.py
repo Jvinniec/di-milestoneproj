cb7b5b87c276eda1936c3ce811f8aa14f18266f0
Josh Cardenzana <jcardenzana@irap.omp.eu>
2020-03-18 22:34:50 +0100

    Update app to pull information from form

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def index():
 def about():
   return render_template('about.html')
 
+@app.route('/index', methods=['POST'])
+def read_form():
+  ret = dict()
+  ret['stocks'] = request.form.get('sname')
+  ret['plotinfo'] = request.form.get('plotinfo')
+  ret['djia']     = bool(request.form.get('djia'))
+  ret['sp500']    = bool(request.form.get('sp500'))
+  ret['nasdaq']   = bool(request.form.get('nasdaq'))
+  print(ret['stocks'])
+  return ret
+
 if __name__ == '__main__':
+  app.debug = True
   app.run(port=33507)
